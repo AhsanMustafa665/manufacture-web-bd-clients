57a85f4294fa3fa7743df38fb6e23261282eae1f
AhsanMustafa665 <ahsanfaisal665@gmail.com>
2022-05-25 23:41:42 +0600

    reviews added

--- src/components/Pages/Dashboard/AddReview.js
+++ src/components/Pages/Dashboard/AddReview.js
@@ -1,32 +1,47 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const AddReview = () => {
   const [addreviews, setAddreviews] = useState([]);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    fetch("http://localhost:5000/reviews", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        alert("success");
+      });
+  };
   let n = 0;
   return (
     <div>
       <h2 className="text-2xl my-9">This is my add review page </h2>
       <div class="overflow-x-auto">
-        <table class="table w-full">
-          <thead>
-            <tr>
-              <th>No.</th>
-              <th>Name</th>
-              <th>Quantity</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {addreviews.map((addre) => (
-              <tr>
-                <td>{n++}</td>
-                <td>{addre.name}</td>
-                <td>{addre.minQuan}</td>
-                <td>{addre.price}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            {...register("ratings", { required: true })}
+            className="border-2 mx-4"
+            type="text"
+          />
+          <input
+            {...register("comments", { required: true })}
+            className="border-2 mx-3"
+            type="text"
+          />
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
